78318635867b7f0ffcec64a2953ad7db811b372b
Ahn, Junho <ahn9807@gmail.com>
2021-07-26 11:02:58 +0000

    add fio

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# bench101
-# bench101
+# Bench101
+## All the benchmarks in one github clone
+
+# File System
+## For testing FIO benchmark sr,sw,rr,rw
+> sudo ./bench./run_bench.sh
+
+# Memory
+## Gapbs (bc,cc,cc_sv,bfs...)
+> sudo ./bench./run_gapbs.sh
+
--- bench/run_fio.sh
+++ bench/run_fio.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-mkdir -p ./result
+mkdir -p ./result/fio
+mkdir -p ./bench
 
-BIN=../fio
+BIN=../fio/fio
 DISABLE_NUMA="numactl -N1 -m1"
 DISABLE_NUMA=""
 CMD_PREFIX="sudo nice -n -20 taskset 1"
+BLOCK_SIZE=4K
+SIZE=4G
 
 echo fio_throughput_sr
-$CMD_PREFIX $DISABLE_NUMA $BIN
+$CMD_PREFIX $DISABLE_NUMA $BIN --rw=read --directory=./bench/ --name fio_test_file --direct=1 -bs=$BLOCK_SIZE --size=$SIZE --numjobs=1 --group_reporting 2>&1 | tee ./result/fio/fio_throughput_sr.output
+
 echo fio_throughput_sw
+$CMD_PREFIX $DISABLE_NUMA $BIN --rw=write --directory=./bench/ --name fio_test_file --direct=1 -bs=$BLOCK_SIZE --size=$SIZE --numjobs=1 --group_reporting 2>&1 | tee ./result/fio/fio_throughput_sr.output
 
 echo fio_throughput_rr
+$CMD_PREFIX $DISABLE_NUMA $BIN --rw=randread --directory=./bench/ --name fio_test_file --direct=1 -bs=$BLOCK_SIZE --size=$SIZE --numjobs=1 --group_reporting 2>&1 | tee ./result/fio/fio_throughput_sr.output
 
 echo fio_throughput_rw
+$CMD_PREFIX $DISABLE_NUMA $BIN --rw=randwrite --directory=./bench/ --name fio_test_file --direct=1 -bs=$BLOCK_SIZE --size=$SIZE --numjobs=1 --group_reporting 2>&1 | tee ./result/fio/fio_throughput_sr.output
 
-echo fio_latency_rw
-
-echo fio_latency_sw
-
-echo fio_latency_rr
-
-echo fio_latency_rw
-
-
+rm -rf ./bench

c0c079c8508b660e3e1102a8d8daddeb3cb157c7
Ahn, Junho <ahn9807@gmail.com>
2021-11-10 05:52:35 +0000

    add redis

---
+++ bench/run_redis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ./result/redis
+
+BIN=../redis/src/
+DISABLE_NUMA="numactl -N1 -m1"
+DISABLE_NUMA=
+
+echo start testing redis
+
+read -p "type remote ip: " ip
+
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/redis-benchmark -h $ip -n 500000 -t Get,Set > ./result/redis/redis.out
---
+++ bench/run_redis_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ../redis/src/redis-server --protected-mode no

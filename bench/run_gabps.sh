74906b1799953d9f499a44dc4e02a99e37fae36b
Ahn, Junho <ahn9807@gmail.com>
2021-12-13 17:40:49 +0000

    X

--- bench/run_gabps.sh
+++ bench/run_gabps.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-mkdir -p ./result/gapbs
+mkdir -p ./result_$1/gapbs
 
 BIN=../gapbs
 DISABLE_NUMA="numactl -N1 -m1"
 DISABLE_NUMA=""
 
+$BIN/converter -g 25 -b 25g.sg
+
 echo start_testing_bc
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/bc -g 25 -n 20 > ./result/gapbs/bc.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/bc -f ./25g.sg -n 5 > ./result_$1/gapbs/bc.output
 
 echo start_testing_bfs
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/bfs -g 25 -n 20 > ./result/gapbs/bfs.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/bfs -f ./25g.sg -n 5 > ./result_$1/gapbs/bfs.output
 
 echo start_testing_cc
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/cc -g 25 -n 20 > ./result/gapbs/cc.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/cc -f ./25g.sg -n 5 > ./result_$1/gapbs/cc.output
 
 echo start_testing_cc_sv
-sudo nice -n -20 taskset 1 $DISALBE_NUMA $BIN/cc_sv -g 25 -n  10 > ./result/gapbs/cc_sv.output
+sudo nice -n -20 taskset 1 $DISALBE_NUMA $BIN/cc_sv -f ./25g.sg -n 5 > ./result_$1/gapbs/cc_sv.output
 
 echo start_testing_pr
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/pr -g 25 -n 20 > ./result/gapbs/pr.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/pr -f ./25g.sg -n 5 > ./result_$1/gapbs/pr.output
 
 echo start_testing_sssp
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/sssp -g 25 -n 20 > ./result/gapbs/sssp.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/sssp -g 25 -n 5 > ./result_$1/gapbs/sssp.output
 
 echo start_testing_tc
-sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/tc -g 20 -n 20 > ./result/gapbs/tc.output
+sudo nice -n -20 taskset 1 $DISABLE_NUMA $BIN/tc -g 20 -n 5 > ./result_$1/gapbs/tc.output
